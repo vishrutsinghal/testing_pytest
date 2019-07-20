8b6f910198a6c226a1254b69da6f3af6d5c62f38
Vishrut Singhal <Vishrut.Singhal@email.com>
2019-07-20 15:30:06 +0530

    Creating calc.py file

---
+++ calc.py
@@ -0,0 +1,37 @@
+# class calc:
+# def __init__(self, value1, value2):
+#     self.value1 = value1
+#     self.value2 = value2
+
+# def add(self):
+#     return self.value1 + self.value2
+
+# def sub(self):
+#     return self.value1 - self.value2
+
+# def mul(self):
+#     return self.value1 * self.value2
+
+# def div(self):
+#     return self.value1 / self.value2
+
+
+# c = calc(100, 20)
+# result = c.sub()
+# print(result)
+
+
+def add(value1, value2):
+    return value1 + value2
+
+
+def sub(value1, value2):
+    return value1 - value2
+
+
+def mul(value1, value2):
+    return value1 * value2
+
+
+def div(value1, value2):
+    return value1 / value2
